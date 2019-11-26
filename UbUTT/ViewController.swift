0022bfc32eb8d75a1b1290561cb875397075aad7
if26-grp3 <if26-grp3@mac07-m102.utt.fr>
2019-11-26 09:50:21 +0100

    Initial Commit

---
+++ UbUTT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UbUTT
+//
+//  Created by if26-grp3 on 26/11/2019.
+//  Copyright © 2019 BourdelatPieret. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
