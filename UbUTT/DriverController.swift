7e3345670b60ad821fda509ed8bc50d5daab622e
if26-grp1 <alexandre.patelli@utt.fr>
2019-12-12 12:23:14 +0100

    Code partiel interface driver

--- UbUTT/DriverController.swift
+++ UbUTT/DriverController.swift
@@ -11,11 +11,20 @@ import UIKit
 class DriverController: UIViewController {
     
     var delegate:isAbleToReceiveData!
+    
+    var user:Utilisateur?
     @IBOutlet weak var stars: UILabel!
     @IBOutlet weak var btEnregistrer: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if (self.user != nil) {
+            //input_modele.setText(user.getConducteur().getModele_voiture());
+            //input_plaque.setText(user.getConducteur().getPlaque_immatriculation());
+            stars.text = String(format:"%.1f", (user!.getConducteur()!.getNoteConducteur()))
+        }
+        
         //HomeActivity -> implémente isAbleToReceiveData
         //implémente fonction pass(Utilisateur)
         //prepare segue -> delegate = self
@@ -26,22 +35,33 @@ class DriverController: UIViewController {
     }
     
     @IBAction func onEditModele(_ sender: UITextField) {
+        btEnregistrer.isEnabled=true
     }
     
     @IBAction func onEditPlaque(_ sender: UITextField) {
+        btEnregistrer.isEnabled=true
     }
     @IBAction func onSave(_ sender: UIButton) {
+        btEnregistrer.isEnabled=false
+        /*if(modeleModified || plaqueModified) {
+            if (input_modele.getText().toString().length() < 1) {
+                input_modele.setBackgroundColor(getResources().getColor(R.color.lightRed));
+            } else {
+                input_modele.setBackgroundColor(Color.WHITE);
+                this.user.getConducteur().setModele_voiture(input_modele.getText().toString());
+            }
+            if (input_plaque.getText().toString().length() < 1) {
+                input_plaque.setBackgroundColor(getResources().getColor(R.color.lightRed));
+            } else {
+                input_plaque.setBackgroundColor(Color.WHITE);
+                this.user.getConducteur().setPlaque_immatriculation(input_plaque.getText().toString().toUpperCase());
+            }
+            this.user.getConducteur().update();
+        }*/
     }
     @IBAction func onReturn(_ sender: UIButton) {
+        delegate.pass(data: user!)
+        dismiss(animated: true, completion: nil)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
